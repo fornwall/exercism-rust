f96bf664221a1268e94b1d7396b42ff67e3dd3d9
Fredrik Fornwall <fredrik@fornwall.net>
2022-11-16 11:10:27 +0100

    Shorten low-power-embedded-game

--- low-power-embedded-game/src/lib.rs
+++ low-power-embedded-game/src/lib.rs
@@ -3,18 +3,12 @@ pub const fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.enumerate().filter_map(
-        |(index, element)| {
-            if index % 2 == 0 {
-                Some(element)
-            } else {
-                None
-            }
-        },
-    )
+    iter.enumerate()
+        .filter_map(|(index, element)| (index % 2 == 0).then_some(element))
 }
 
 pub struct Position(pub i16, pub i16);
+
 impl Position {
     pub const fn manhattan(&self) -> i16 {
         self.0.abs() + self.1.abs()
