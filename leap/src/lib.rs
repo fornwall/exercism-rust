4e54a033a8401f9c8372c4e2eb443cae346aae01
Fredrik Fornwall <fredrik@fornwall.net>
2022-11-17 17:23:17 +0100

    Add leap

---
+++ leap/src/lib.rs
@@ -0,0 +1,7 @@
+pub const fn is_leap_year(year: u64) -> bool {
+    let divisible_by_4 = year % 4 == 0;
+    let divisible_by_100 = year % 100 == 0;
+    let divisible_by_400 = year % 400 == 0;
+
+    divisible_by_4 && !(divisible_by_100 ^ divisible_by_400)
+}
