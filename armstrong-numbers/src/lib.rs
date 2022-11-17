8696d24e91721527d49fd5f667c6a14f868c4247
Fredrik Fornwall <fredrik@fornwall.net>
2022-11-17 16:53:11 +0100

    Add armstrong-numbers

---
+++ armstrong-numbers/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let num_str = num.to_string();
+    let num_digits = num_str.len() as u32;
+    num_str
+        .bytes()
+        .map(|digit| (u32::from(digit - b'0')).pow(num_digits))
+        .sum::<u32>()
+        == num
+}
