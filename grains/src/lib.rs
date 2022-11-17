33e674d969f32a6b04db8d866c6828e1e5a6b850
Fredrik Fornwall <fredrik@fornwall.net>
2022-11-17 17:30:17 +0100

    Add grains

---
+++ grains/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn square(s: u32) -> u64 {
+    assert!((1..=64).contains(&s), "Square must be between 1 and 64");
+    2_u64.pow(s - 1)
+}
+
+pub fn total() -> u64 {
+    (1..=64).map(square).sum()
+}
