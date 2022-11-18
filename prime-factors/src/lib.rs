eaf758ddd4160d48417a45e736b58d7bd5af117c
Fredrik Fornwall <fredrik@fornwall.net>
2022-11-18 10:19:41 +0100

    prime-factors

---
+++ prime-factors/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn factors(n: u64) -> Vec<u64> {
+    let mut primes = Vec::new();
+    let mut remainder = n;
+    let mut p = 2;
+
+    while remainder >= p {
+        if remainder % p == 0 {
+            primes.push(p);
+            remainder /= p;
+        } else {
+            p += 1;
+        }
+    }
+
+    primes
+}
