a7bd749866164bf6e6ec697586276e80c264e711
Fredrik Fornwall <fredrik@fornwall.net>
2022-11-18 10:30:04 +0100

    Add proverb

---
+++ proverb/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    if list.is_empty() {
+        return "".to_string();
+    }
+
+    let wants = list
+        .windows(2)
+        .map(|words| format!("For want of a {} the {} was lost.\n", words[0], words[1]))
+        .collect::<String>();
+
+    format!("{wants}And all for the want of a {}.", list[0])
+}
